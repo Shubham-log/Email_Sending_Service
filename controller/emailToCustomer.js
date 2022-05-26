1d909739d3684d05facb595cbb2f7d002249f935
Shubham Sharma <shubham5.logistics@gmail.com>
2022-05-26 17:15:26 +0530

    all working fine

--- controller/emailToCustomer.js
+++ controller/emailToCustomer.js
@@ -2,43 +2,44 @@ const dotenv = require("dotenv");
 
 dotenv.config({path:"config/config.env"});
 
+// e7cca9b2eba2383a57509b5402bdc1ce
+
 const mailjet = require('node-mailjet').connect(
-    process.env.MJ_APIKEY_PUBLIC,
-    process.env.MJ_APIKEY_PRIVATE
+    "23b358226bfd92dd0a2db33ae9c8f521",
+    "eb07c5ce4f1ec946731f0faebdc70d32"
     )
 
-emailToCustomer= (req,res)=>{
+nextservice= (req,res)=>{
 
-  console.log(Object.keys[res])
-    // const request = mailjet.post('send', { version: 'v3.1' }).request({
-    //     Messages: [
-    //       {
-    //         From: {
-    //           Email: 'brettalex174@gmail.com',
-    //           Name: 'Reply Department',
-    //         },
-    //         To: [
-    //           {
-    //             Email: 'shubham5.logistics@gmail.com',
-    //             Name: 'Shubham Sharma',
-    //           },
-    //         ],
-    //         Subject: "demo",
-    //         TextPart: "demo",
-    //         HTMLPart:
-    //           '<h3>Dear passenger 1, welcome to <a href="https://www.mailjet.com/">Mailjet</a>!</h3><br />May the delivery force be with you!',
-    //       },
-    //     ],
-    //   })
-    //   request
-    //     .then(result => {
-    //       res.send(220);
-    //     })
-    //     .catch(err => {
-    //       res.send(223);
-    //     })
+    const request = mailjet.post('send', { version: 'v3.1' }).request({
+        Messages: [
+          {
+            From: {
+              Email: 'shubham5.logistics@outlook.com',
+              Name: 'Shubham Sharma',
+            },
+            To: [
+              {
+                Email: req.email,
+                Name: req.name,
+              },
+            ],
+            Subject: "Reply Department Shubham's Team !!",
+            TextPart: "",
+            HTMLPart:
+              '<h3>THANK YOU !! FOR CONTACTING US WE WILL GET BACK TO YOU IN DUE COURSE OF TIME CHECK OUT SOME OF WORKS HERE<a href="https://shubhams-portfolio.netlify.app/"></a>!</h3>',
+          },
+        ],
+      })
+      request
+        .then(result => {
+          return res.sendStatus(200);
+        })
+        .catch(err => {
+          return res.sendStatus(223);
+        })
      
 }
 
-module.exports=emailToCustomer;
+module.exports=nextservice;
 
--- controller/sendEmailController.js
+++ controller/sendEmailController.js
@@ -4,6 +4,7 @@ const asyncerror = require("../middleware/catchAsyncError");
 const dotenv = require("dotenv");
 const emailToCustomer = require("./emailToCustomer")
 
+const nextservice = require("./emailToCustomer")
 
 dotenv.config({path:"config/config.env"}); //to connect env folder
 
@@ -14,7 +15,7 @@ const mailjet = require('node-mailjet').connect(
   )
 // process.env.MJ_APIKEY_PUBLIC,
     // process.env.MJ_APIKEY_PRIVATE
-exports.sendEmailtoCustomer = (req,res,emailToCustomer)=>{
+exports.sendEmailtoCustomer = (req,res)=>{
     const details={
       ...req.body
     }
@@ -33,18 +34,15 @@ exports.sendEmailtoCustomer = (req,res,emailToCustomer)=>{
             ],
             Subject: details.subject,
             TextPart: details.message,
-            HTMLPart:
-              '<h3>Dear passenger 1, welcome to <a href="https://www.mailjet.com/">Mailjet</a>!</h3><br />May the delivery force be with you!',
           },
         ],
       })
       request
         .then(result => {
-          // emailToCustomer(detalis,res);
-          res.sendStatus(200)
+         return  nextservice(details,res)
         })
         .catch(err => {
           res.sendStatus(404)
         })
-
+    
 }
